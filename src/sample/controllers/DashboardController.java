9712cd844d26d3ea83630e264b4b9e1bde0a114a
idev46 <zaibekhan2019@gmail.com>
2022-01-04 01:35:00 +0500

    main dashboard added

---
+++ src/sample/controllers/DashboardController.java
@@ -0,0 +1,17 @@
+package sample.controllers;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+    public Label totalConsumersLabel;
+    public Label energyLabel;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
